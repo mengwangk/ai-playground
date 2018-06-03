cadd8c028e75dcae0a11c8bf3bab0adf35833ac6
Koh Meng Wang <mengwangk@gmail.com>
2018-06-03 12:45:42 +0800

    Updated files

--- source/source_test.py
+++ source/source_test.py
@@ -4,35 +4,34 @@ Tests for the source module.
 
 import unittest
 import datetime as dt
-import time as tm
 
 from source import YahooFinanceSource
 
 class SourceTest(unittest.TestCase):
 
-    def test_get_stocks(self):
-        print("Getting the stocks")
-        symbol = '6742.KL'
+    _TEST_SYMBOL = '6742.KL'
 
-        today = dt.datetime.today().strftime('%Y-%m-%d')
+    _YAHOO_FINANCE_SOURCE = YahooFinanceSource(_TEST_SYMBOL)
+    _TODAY  = dt.datetime.today().strftime('%Y-%m-%d')
 
-        yahoo_finance_source = YahooFinanceSource(symbol)
+    def test_get_stock_prices(self):
+        print("Getting historical prices")
 
-        # Get historical dividends
+
+        # Get historical stock data
+        # historical_data = self._YAHOO_FINANCE_SOURCE.get_historical_stock_data('2016-05-15', self._TODAY, 'daily')
+        # prices = historical_data[self._TEST_SYMBOL]['prices']
+        # print(prices)
+        # for price in prices:
+        #     print(price.get('close', None))
 
 
         # Get current price
         # current_price = yahoo_finance_source.get_current_price()
         # print(current_price)
 
-        # Get historical prices
-        # historical_data = yahoo_finance_source.get_historical_stock_data('2017-05-15', today, 'daily')
-        # prices = historical_data[symbol]['prices']
-        # print(prices)
-        # for price in prices:
-        #     print(price.get('close', None))
 
-
-
-
-
+    def test_get_dividend_history(self):
+        print("Getting historical dividends")
+        dividend_data = self._YAHOO_FINANCE_SOURCE.get_historical_stock_dividend_data('2017-05-15', self._TODAY, 'daily')
+        print(dividend_data)
--- source/yahoofinance.py
+++ source/yahoofinance.py
@@ -5,7 +5,13 @@ Version: 0.0.1
 =======================
 """
 
+import re
+import requests
+import time
+from json import loads
+from bs4 import BeautifulSoup
 from yahoofinancials import YahooFinancials
+
 
 # Yahoo Finance data source
 class YahooFinanceSource(YahooFinancials):
@@ -13,6 +19,76 @@ class YahooFinanceSource(YahooFinancials):
     def __init__(self, ticker):
         super(YahooFinanceSource, self).__init__(ticker)
 
-    # Public method to get historical dividends
-    def get_historical_dividends(self):
-        print("Get dividends history")
+    # private static method to scrap data from yahoo finance
+    @staticmethod
+    def _scrape_dividend_data(url, tech_type, statement_type):
+        response = requests.get(url)
+        time.sleep(7)
+        soup = BeautifulSoup(response.content, "html.parser")
+        script = soup.find("script", text=re.compile("root.App.main")).text
+        data = loads(re.search("root.App.main\s+=\s+(\{.*\})", script).group(1))
+        stores = data["context"]["dispatcher"]["stores"]["HistoricalPriceStore"]
+        return stores
+
+
+    # Private Method to clean the dates of the newly returns historical stock data into readable format
+    def _clean_historical_div_data(self, hist_data):
+        data = {}
+        for k, v in hist_data.items():
+            print('k {} v {}'.format(k,v))
+            if 'date' in k.lower():
+                cleaned_date = self.format_date(v, 'standard')
+                dict_ent = {k: {u'' + 'formatted_date': cleaned_date, 'date': v}}
+                data.update(dict_ent)
+            elif isinstance(v, list):
+                sub_dict_list = []
+                for sub_dict in v:
+                    sub_dict[u'' + 'formatted_date'] = self.format_date(sub_dict['date'], 'standard')
+                    sub_dict_list.append(sub_dict)
+                dict_ent = {k: sub_dict_list}
+                data.update(dict_ent)
+            else:
+                dict_ent = {k: v}
+                data.update(dict_ent)
+        return data
+
+
+    # Private method to get time interval code
+    def _build_historical_dividend_url(self, ticker, hist_oj, filter='div'):
+        url = self._BASE_YAHOO_URL + ticker + '/history?period1=' + str(hist_oj['start']) + '&period2=' + \
+              str(hist_oj['end']) + '&interval=' + hist_oj['interval'] + '&filter=' + filter + '&frequency=' + \
+              hist_oj['interval']
+        return url
+
+
+    # Private Method to take scrapped data and build a data dictionary with
+    def _create_dict_ent_div(self, ticker, statement_type, tech_type, report_name, hist_obj):
+        up_ticker = ticker.upper()
+        YAHOO_URL = self._build_historical_dividend_url(up_ticker, hist_obj)
+        print(YAHOO_URL)
+        re_data = self._scrape_dividend_data(YAHOO_URL, tech_type, statement_type)
+        cleaned_re_data = self._clean_historical_div_data(re_data)
+        dict_ent = {up_ticker: cleaned_re_data}
+        return dict_ent
+
+
+    # Public Method for user to get historical stock dividend data
+    def get_historical_stock_dividend_data(self, start_date, end_date, time_interval):
+        interval_code = self.get_time_code(time_interval)
+        start = self.format_date(start_date, 'unixstamp')
+        end = self.format_date(end_date, 'unixstamp')
+        hist_obj = {'start': start, 'end': end, 'interval': interval_code}
+        data = self.get_stock_data('history', hist_obj=hist_obj)
+
+
+    # Public Method to get stock data
+    def get_stock_dividend_data(self, statement_type='history', tech_type='', report_name='', hist_obj={}):
+        data = {}
+        if isinstance(self.ticker, str):
+            dict_ent = self._create_dict_ent_div(self.ticker, statement_type, tech_type, report_name, hist_obj)
+            data.update(dict_ent)
+        else:
+            for tick in self.ticker:
+                dict_ent = self._create_dict_ent_div(tick, statement_type, tech_type, report_name, hist_obj)
+                data.update(dict_ent)
+        return data
