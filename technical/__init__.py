f236c1e2a76a5596085b4be3cb36926ebb82a93c
Koh Meng Wang <mengwangk@gmail.com>
2018-06-21 21:44:39 +0800

    Updated files

--- technical/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""It is a technical analysis library to financial time series datasets.
-You can use it to do feature engineering from financial datasets. It is builded
-on pandas python library.
-
-"""
-from .wrapper import *
-from .utils import *
