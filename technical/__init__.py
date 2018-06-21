b7ada8d61ee6c308cd831bf6523e953052488c3c
Koh Meng Wang <mengwangk@gmail.com>
2018-06-21 21:18:03 +0800

    Updated files

---
+++ technical/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""It is a technical analysis library to financial time series datasets.
+You can use it to do feature engineering from financial datasets. It is builded
+on pandas python library.
+
+"""
+from .wrapper import *
+from .utils import *
---
+++ technical/utils.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import math
+
+
+def dropna(df):
+    """Drop rows with "Nans" values
+    """
+    df = df[df < math.exp(709)] # big number
+    df = df[df != 0.0]
+    df = df.dropna()
+    return df
