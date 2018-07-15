830af4dfb058e6f69a9220e3cdbd5cebcb8ada8c
Koh Meng Wang <mengwangk@gmail.com>
2018-07-15 14:29:39 +0800

    Updated files

---
+++ source/scrapy/klse/klse/spiders/dividend_history.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# Get stocks dividend histories
+
+import scrapy
+
+
+class DividendHistorySpider(scrapy.Spider):
+    """ Dividend history scraper s"""
+    name = "dividend_history"
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
