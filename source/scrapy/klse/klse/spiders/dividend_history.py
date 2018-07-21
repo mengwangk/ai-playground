e9edeab19e114e50972fc608912ffcd96848158f
Koh Meng Wang <mengwangk@gmail.com>
2018-07-21 14:53:21 +0800

    Updated files

--- source/scrapy/klse/klse/spiders/dividend_history.py
+++ source/scrapy/klse/klse/spiders/dividend_history.py
@@ -17,6 +17,7 @@ class DividendHistorySpider(scrapy.Spider):
     def start_requests(self):
         with open(self._TICKER_FILE, 'r') as f:
             reader = csv.reader(f)
+            next(reader, None)  # skip the headers
             stock_list = list(reader)
 
         urls = []
@@ -27,4 +28,17 @@ class DividendHistorySpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        print("parsing response")
+        code = response.request.url.split('/')[-1].split('.')[0]
+        ann_dates = response.xpath('//*[@id="entitlementTable"]/tbody/tr/td[1]/text()').extract()
+        ex_dates = response.xpath('//*[@id="entitlementTable"]/tbody/tr/td[2]/text()').extract()
+        payment_dates = response.xpath('//*[@id="entitlementTable"]/tbody/tr/td[3]/text()').extract()
+        dividend_type = response.xpath('//*[@id="entitlementTable"]/tbody/tr/td[4]/text()').extract()
+        subject = response.xpath('//*[@id="entitlementTable"]/tbody/tr/td[5]/text()').extract()
+        amount = response.xpath('//*[@id="entitlementTable"]/tbody/tr/td[6]/text()').extract()
+
+        dividends = [ [code] + list(d) for d in zip(ann_dates, ex_dates, payment_dates, dividend_type, subject, amount)]
+        print(dividends)
+        with open('KLSE_dividends.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(['code', 'ann_date','ex_date','payment_date','dividend_type','subject', 'amount'])
+            writer.writerows(dividends)
