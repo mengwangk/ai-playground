12ae50799a2143aa5b44a12eb9670f0442b01fcf
Koh Meng Wang <mengwangk@gmail.com>
2018-10-22 23:54:10 +0800

    Updated files

---
+++ stock_history.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+""" Download and preprocess stock historical prices.
+
+"""
+
+
+class Stock
---
+++ strategy/test_strategy_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""TA library testing
+
+https://github.com/bukosabino/ta
+
+"""
+
+import pandas as pd
+from ta import *
+
+
+#
